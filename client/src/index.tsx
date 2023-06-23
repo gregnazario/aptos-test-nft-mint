79e92ad75556622ea963bb102efbf3f0e59557af
Greg Nazario <greg@gnazar.io>
2023-06-23 12:02:48 -0700

    add all wallets supported by wallet adapter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,27 +3,51 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RiseWallet } from "@rise-wallet/wallet-adapter";
-import { PetraWallet } from "petra-plugin-wallet-adapter";
-import { MartianWallet } from "@martianwallet/aptos-wallet-adapter";
-import { PontemWallet } from "@pontem/wallet-adapter-plugin";
+import {BloctoWallet} from "@blocto/aptos-wallet-adapter-plugin";
+import {FewchaWallet} from "fewcha-plugin-wallet-adapter";
+import {MartianWallet} from "@martianwallet/aptos-wallet-adapter";
+import {NightlyWallet} from "@nightlylabs/aptos-wallet-adapter-plugin";
+import {OpenBlockWallet} from "@openblockhq/aptos-wallet-adapter";
+import {PetraWallet} from "petra-plugin-wallet-adapter";
+import {PontemWallet} from "@pontem/wallet-adapter-plugin";
+import {RiseWallet} from "@rise-wallet/wallet-adapter";
+import {TokenPocketWallet} from "@tp-lab/aptos-wallet-adapter";
+import {TrustWallet} from "@trustwallet/aptos-wallet-adapter";
+import {MSafeWalletAdapter} from "msafe-plugin-wallet-adapter";
+import {WelldoneWallet} from "@welldone-studio/aptos-wallet-adapter";
 import {
     AptosWalletAdapterProvider,
+    NetworkName,
 } from "@aptos-labs/wallet-adapter-react";
-import { FewchaWallet } from "fewcha-plugin-wallet-adapter";
-import { MSafeWalletAdapter } from "msafe-plugin-wallet-adapter";
 
 
-const wallets = [new PetraWallet(), new MartianWallet(), new PontemWallet(), new RiseWallet(), new FewchaWallet(), new MSafeWalletAdapter()];
+const wallets = [
+    // Blocto supports Testnet/Mainnet for now.
+    new BloctoWallet({
+        network: NetworkName.Testnet,
+        bloctoAppId: "6d85f56e-5f2e-46cd-b5f2-5cf9695b4d46",
+    }),
+    new FewchaWallet(),
+    new MartianWallet(),
+    new MSafeWalletAdapter(),
+    new NightlyWallet(),
+    new OpenBlockWallet(),
+    new PetraWallet(),
+    new PontemWallet(),
+    new RiseWallet(),
+    new TokenPocketWallet(),
+    new TrustWallet(),
+    new WelldoneWallet(),
+];
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-      <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
-          <App />
-      </AptosWalletAdapterProvider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <AptosWalletAdapterProvider plugins={wallets} autoConnect={true}>
+            <App/>
+        </AptosWalletAdapterProvider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
